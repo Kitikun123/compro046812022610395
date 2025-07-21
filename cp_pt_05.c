d8a59e185868431e0d83fed0f5d6c17e360e9ab7
Kitikun123 <kitikuntongrat@gmail.com>
2025-07-21 13:42:35 +0700

    Add files via upload

---
+++ cp_pt_05.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float weight;
+    printf("Enter your weight: ");
+    scanf("%f", &weight);
+    printf("Your weight is %.2f kg.\n", weight);
+    return 0;
+
+}
